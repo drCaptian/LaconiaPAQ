a5510f7f15ef17053b3d0d7e140c4054326e9115
drxxxfuzz <style444bastard@gmail.com>
2026-03-07 16:29:05 +0500

    Fix for more creating exampler of compressor

--- LaconiaPAQ/LaconiaPAQ/PAQCompressor.cs
+++ LaconiaPAQ/LaconiaPAQ/PAQCompressor.cs
@@ -6,40 +6,15 @@ namespace LaconiaPAQ;
 public class PAQCompressor
 {
     private readonly string _toAction;
-    private readonly LZP lzp;
-    private readonly Predictor predictor;
+    private readonly LZP _lzp;
+    private readonly Predictor _predictor;
 
-    public PAQCompressor(string fileToAction, string pathToSaveResult, CompressorMode mode)
+    public PAQCompressor(string fileToAction)
     {
         _toAction = fileToAction;
-        lzp = new LZP();
-        predictor = new Predictor();
-        switch (mode)
-        {
-            case CompressorMode.Compress :
-                const string allowedType = ".paq";
-                if (pathToSaveResult.EndsWith(allowedType, StringComparison.OrdinalIgnoreCase))
-                {
-                    Compress(pathToSaveResult);
-                }
-                else
-                {
-                    throw new Exception("Wrong type in output file");
-                }
-                break;
-            case CompressorMode.Decompress:
-                if (fileToAction.EndsWith(allowedType, StringComparison.OrdinalIgnoreCase))
-                {
-                    Decompress(pathToSaveResult);
-                }
-                else
-                {
-                    throw new Exception("Wrong type in input file");
-                }
-                break;
-            default:
-                throw new Exception("Non-valid mode compressor");
-        }
+        _lzp = new LZP();
+        _predictor = new Predictor();
+        
     }
 
     public void Compress(string output)
@@ -51,10 +26,10 @@ public class PAQCompressor
             int b;
             while ((b = inFile.ReadByte()) != -1)
             {
-                int p = predictor.Predict(0);
+                int p = _predictor.Predict(0);
                 encoder.EncodeBit(b & 1);
-                lzp.Update(b);
-                predictor.Update(0, b);
+                _lzp.Update(b);
+                _predictor.Update(0, b);
             }
             encoder.Flush();
         }
